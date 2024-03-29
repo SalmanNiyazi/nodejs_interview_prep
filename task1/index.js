db33fed94c5459957c6288002d60afc4dfd420f7
Salman Niyazi <salmanniyazi1@gmail.com>
2024-03-30 00:32:07 +0530

    task1 done and started task2

---
+++ task1/index.js
@@ -0,0 +1,7 @@
+/*
+Create a Basic HTTP Server:
+Write a Node.js script to create a simple HTTP server that responds to requests with a "Hello, World!" message.
+How would you modify this server to serve JSON data instead?
+ */
+
+
